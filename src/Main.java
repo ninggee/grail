f8da3bbf0ee697ee3aae3e17cec836ee8f248458
ningge <weiguangningtian@gmail.com>
2018-03-27 10:15:05 +0800

    init project

---
+++ src/Main.java
@@ -0,0 +1,56 @@
+import soot.*;
+import soot.jimple.JimpleBody;
+import soot.jimple.Stmt;
+import soot.options.Options;
+import soot.tagkit.LineNumberTag;
+import soot.toolkits.graph.BriefUnitGraph;
+import soot.toolkits.graph.ExceptionalUnitGraph;
+import soot.toolkits.graph.UnitGraph;
+import soot.util.Chain;
+import util.Functions;
+import util.PetriNet;
+
+import java.util.Collections;
+import java.util.Iterator;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String path = "D:\\codes\\java\\Grail\\src";
+        Functions.initSoot(path);
+
+        SootClass appclass = Scene.v().loadClassAndSupport("examples.account.Account");
+
+        //appclass.get
+
+        SootMethod method = appclass.getMethodByName("transfer");
+
+        printCallFromMethod(method);
+        //UnitGraph unitGraph = new ExceptionalUnitGraph(method.retrieveActiveBody());
+        //System.out.print(unitGraph.size());
+        //
+        //PetriNet petriNet = new PetriNet(method.getName(), unitGraph);
+        //
+        //System.out.println(petriNet);
+    }
+
+    public static void printCallFromMethod(SootMethod method) {
+        JimpleBody jimpleBody = (JimpleBody)method.retrieveActiveBody();
+
+        UnitGraph g = new BriefUnitGraph(jimpleBody);
+
+        Iterator<Unit> it = g.iterator();
+
+        while(it.hasNext()) {
+            Stmt stmt = (Stmt)it.next();
+            LineNumberTag lineNumberTag = (LineNumberTag)stmt.getTag("LineNumberTag");
+            if(stmt.containsInvokeExpr()) {
+                //System.out.println("the invoke is : " + stmt.toString());
+                if (lineNumberTag != null) {
+                    System.out.print("In line " + lineNumberTag.getLineNumber());
+                    System.out.println(" call method : " + stmt.getInvokeExpr().getMethod());
+                }
+            }
+        }
+    }
+}
