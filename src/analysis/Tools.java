b1448631711884d7577336c6d4fe5e989efea801
ningge <weiguangningtian@gmail.com>
2018-04-05 21:44:40 +0800

    add containing method

---
+++ src/analysis/Tools.java
@@ -0,0 +1,118 @@
+package analysis;
+
+import soot.*;
+import soot.jimple.JimpleBody;
+import soot.jimple.Stmt;
+import soot.jimple.toolkits.callgraph.CHATransformer;
+import soot.jimple.toolkits.callgraph.CallGraph;
+import soot.jimple.toolkits.callgraph.Sources;
+import soot.options.Options;
+import soot.tagkit.LineNumberTag;
+import soot.toolkits.graph.BriefUnitGraph;
+import soot.toolkits.graph.UnitGraph;
+
+import java.io.PrintStream;
+import java.util.*;
+
+public class Tools {
+    public static CallSite containMethod = null;
+
+    public static void getContainMethod(String path, final String className, int line) {
+
+        __init();
+
+        String argsString = "-cp .;" + path + " -pp -validate " + className;
+
+
+        PackManager.v().getPack("wjtp").add(new Transform("wjtp.myTrans", new SceneTransformer() {
+
+            @Override
+            protected void internalTransform(String phaseName, Map options) {
+
+                SootClass app = Scene.v().loadClassAndSupport(className);
+
+                Iterator<SootMethod> methodIterator = app.methodIterator();
+
+                while(methodIterator.hasNext()) {
+                    SootMethod m = (SootMethod)methodIterator.next();
+
+                    if(isContain(m, line)) {
+                        containMethod = new CallSite(line, app.getName(), m.getSignature());
+                        break;
+                    }
+                }
+            }
+        }));
+
+        soot.Main.main(argsString.split(" "));
+    }
+
+    // 初始化soot参数
+    private static void __init() {
+        soot.G.reset();
+
+        Options options = Options.v();
+        //allow soot class creation from missing classes
+        options.set_allow_phantom_refs(true);
+
+        //prepend the given soot classpath to the default classpath
+        //options.set_prepend_classpath(true);
+
+        // run internal validation on bodies
+        options.set_validate(true);
+
+        // set output format for soot
+        options.set_output_format(Options.output_format_none);
+
+        // only java are accepted by soot analysis
+        options.set_src_prec(Options.src_prec_class);
+
+        // keep line number table, so you can access line number when later analysis
+        options.set_keep_line_number(true);
+
+        // attach bytecode offset to IR, we don't need this feature this project
+        //options.set_keep_offset(true);
+
+        // do not load bodies for excluded classes
+        options.set_no_bodies_for_excluded(true);
+
+        options.set_whole_program(true);
+    }
+
+    private static boolean isContain(SootMethod m, int line) {
+        JimpleBody jimpleBody = (JimpleBody)m.retrieveActiveBody();
+
+        UnitGraph g = new BriefUnitGraph(jimpleBody);
+
+        List<Unit> heads = g.getHeads();
+
+        Unit head = heads.get(0);
+        LineNumberTag lineNumberTag = (LineNumberTag)head.getTag("LineNumberTag");
+
+        int start = lineNumberTag.getLineNumber();
+
+        List<Unit> tails = g.getTails();
+
+        Unit tail = tails.get(0);
+
+        lineNumberTag = (LineNumberTag)tail.getTag("LineNumberTag");
+
+        int end = lineNumberTag.getLineNumber();
+
+        return line >= start && line <= end;
+
+
+    }
+
+    public static void main(String[] args) {
+            String path = "D:\\codes\\java\\scheduler\\scheduler\\src\\examples\\linkedlist\\bin";
+            getContainMethod(path, "linkedlist.BugTester", 15);
+
+            System.out.println(containMethod.getSignature());
+    }
+
+
+
+
+
+}
